0847a1e0576fba4c69bd8dc19a85fb7e62f45d41
manikbadsha <52635754+manikbadsha@users.noreply.github.com>
2025-10-25 23:08:11 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	});
 
 	var typed=new Typed(".typed",{
-		strings:["Software Developer.","Survey Programmer & Data Analyst."],
+		strings:["Software Developer.","Survey Programmer."],
 		typeSpeed:70,
 		loop:true,
 		startDelay:1000,
